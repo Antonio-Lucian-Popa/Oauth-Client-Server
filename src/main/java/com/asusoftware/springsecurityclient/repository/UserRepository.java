ce8f637a00eeea7cf7dd1c904a5fa2088a624711
AsuSoftware <asu.software.dev@gmail.com>
2022-11-03 12:32:21 +0200

    Initial commit

---
+++ src/main/java/com/asusoftware/springsecurityclient/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.asusoftware.springsecurityclient.repository;
+
+import com.asusoftware.springsecurityclient.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
